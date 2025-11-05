8610a95e1f6d54f9a9069998ddc4f14f72085a6a
IlyaGrigor <birthdayay371@gmail.com>
2025-11-05 15:27:11 +0200

    lesson04_task3FIX02

--- lesson-04/03.js
+++ lesson-04/03.js
@@ -18,7 +18,14 @@ findCommonElements([1, 2, 3], [2, 3, 4]) // [2, 3]
 */
 
 const superArray02 = [1, 2, 3, 4, 5, 6];
-const superArray03 = [44, 9, 23, 10, 66, 8];
+const superArray03 = [11, 9, 1, 10, 66];
+
+const superArray04 = [1, 2, 3, 4, 5, 6];
+const superArray05 = [4, 8, 9, 10, 11, 12, 13]
+
+const superArray06 = [33, 44, 55];
+const superArray07 = [66, 77, 88];
+
 
 function includesElement(array, element) {
     // return array.includes(element);
@@ -44,3 +51,9 @@ function findCommonElements(array1, array2) {
 
 const elementsFounded = findCommonElements(superArray02, superArray03);
 console.log(elementsFounded)
+
+const elements2Founded = findCommonElements(superArray04, superArray05);
+console.log(elements2Founded)
+
+const elements3Founded = findCommonElements(superArray06, superArray07);
+console.log(elements3Founded)
