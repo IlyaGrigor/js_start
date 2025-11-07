45ddaf20ceeda7b9141a1519fdc24ec8a0591937
IlyaGrigor <birthdayay371@gmail.com>
2025-11-07 15:25:55 +0200

    lesson04-task3FINALFIXHOPESO

--- lesson-04/03.js
+++ lesson-04/03.js
@@ -17,43 +17,63 @@ findCommonElements([1, 2, 3], [2, 3, 4]) // [2, 3]
 Подсказка: можно использовать функцию `includesElement`, которую мы написали ранее. Переписывать её не нужно, она доступна по всему проекту за счёт hoisting.
 */
 
-const superArray02 = [1, 2, 3, 4, 5, 6];
-const superArray03 = [11, 4, 1, 10, 66];
+// const superArray02 = [1, 2, 3, 4, 5, 6];
+// const superArray03 = [11, 4, 1, 10, 66];
+//
+// const superArray04 = [4, 8, 9, 10, 11, 12, 6, 8];
+// const superArray05 = [1, 2, 3, 4, 5, 6, 7];
+//
+// const superArray06 = [33, 44, 55];
+// const superArray07 = [66, 77, 88];
 
-const superArray04 = [4, 8, 9, 10, 11, 12, 6, 8];
-const superArray05 = [1, 2, 3, 4, 5, 6, 7];
 
-const superArray06 = [33, 44, 55];
-const superArray07 = [66, 77, 88];
-
-
-function includesElement(array, element) {
-    // return array.includes(element);
-    let result;
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] === element) {
-            return result = true;
-        }
-    }
-    return result = false;
-}
+// function includesElement(array, element) {
+//     // return array.includes(element);
+//     let result;
+//     for (let i = 0; i < array.length; i++) {
+//         if (array[i] === element) {
+//             return result = true;
+//         }
+//     }
+//     return result = false;
+// }
 
 
 function findCommonElements(array1, array2) {
     const newArray = [];
-    for (let i = 0; i < Math.max(array1.length, array2.length); i++) {
-        if (includesElement(array1, array2[i]) === true) {
-            newArray.push(array2[i]);
+    if (array1.length > array2.length) {
+        for (let i = 0; i < array1.length; i++) {
+            if (includesElement(array1, array2[i]) === true) {
+                newArray.push(array2[i]);
+            }
         }
+    } else if (array1.length < array2.length) {
+        for (let i = 0; i < array2.length; i++) {
+            for (let i = 0; i < array1.length; i++) {
+                if (includesElement(array2, array1[i]) === true) {
+                    newArray.push(array1[i]);
+                }
+            }
+        }
+    } else {
+        for (let i = 0; i < array2.length; i++) {
+            for (let i = 0; i < array1.length; i++) {
+                if (includesElement(array2, array1[i]) === true) {
+                    newArray.push(array1[i]);
+                }
+            }
+        }
+
+        // return newArray;
     }
     return newArray;
 }
 
-const elementsFounded = findCommonElements(superArray02, superArray03);
-console.log(elementsFounded)
-
-// const elements2Founded = findCommonElements(superArray04, superArray05);
-// console.log(elements2Founded)
-//
-// const elements3Founded = findCommonElements(superArray06, superArray07);
-// console.log(elements3Founded)
+    // const elementsFounded = findCommonElements(superArray02, superArray03);
+    // console.log(elementsFounded);
+    //
+    // const elements2Founded = findCommonElements(superArray04, superArray05);
+    // console.log(elements2Founded)
+    //
+    // const elements3Founded = findCommonElements(superArray06, superArray07);
+    // console.log(elements3Founded)
