0434546e13cdd0e0f87e90a6435deca31fbb259f
IlyaGrigor <birthdayay371@gmail.com>
2025-12-01 19:15:36 +0200

    lesson-08_task2

--- lesson-08/02.js
+++ lesson-08/02.js
@@ -4,11 +4,18 @@
  */
 
 function isNumeric(str) {
-  // your code
+    let string = str.trim()
+    if (string === "") {
+        return false;
+    }
+    const num = Number(string);
+    return !Number.isNaN(num);
 }
 
 // console.log(isNumeric("123")) // Ожидаемый результат: true
 // console.log(isNumeric("12.3")) // Ожидаемый результат: true
 // console.log(isNumeric("123abc")) // Ожидаемый результат: false
-// console.log(isNumeric("abc")) // Ожидаемый результат: false
+// console.log(isNumeric("1g3")) // Ожидаемый результат: false
 // console.log(isNumeric(" ")) // Ожидаемый результат: false
+
+
